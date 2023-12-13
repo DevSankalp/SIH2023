a5d721ccf91256a53545aae57a582522122fbe37
DevSankalp <94849180+DevSankalp@users.noreply.github.com>
2023-12-14 05:25:31 +0530

    yarn removed

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,28 +1,21 @@
 import React, { useState } from "react";
 import Navbar from "../Components/navbar";
 import ParticleSphere from "../Components/particleSphere";
-import {  signInWithEmailAndPassword   } from 'firebase/auth';
-import { auth } from "../firebase"; 
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 import {
   createUserWithEmailAndPassword,
-  updateProfile, sendPasswordResetEmail,
+  updateProfile,
+  sendPasswordResetEmail,
 } from "firebase/auth";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-
-var background_color = "#f2ebfb";
-var cont_color = "#240046";
-
-
-
 const AuthForm = ({ formType, handleSubmit, background }) => {
-
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('')
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
-  
 
   // handleRegister
   const handleRegister = async (event, name, email, password) => {
@@ -30,13 +23,15 @@ const AuthForm = ({ formType, handleSubmit, background }) => {
 
     try {
       await register(name, email, password);
-      setEmail('');setName('');setPassword('')
+      setEmail("");
+      setName("");
+      setPassword("");
       // Show success notification
-      alert('Registration successful!');
-      handleLogin(event,email,password);
+      alert("Registration successful!");
+      handleLogin(event, email, password);
     } catch (error) {
       // Show error notification
-      alert('Registration failed. Please try again.');
+      alert("Registration failed. Please try again.");
       console.error(error);
     }
   };
@@ -50,56 +45,65 @@ const AuthForm = ({ formType, handleSubmit, background }) => {
     }
   };
 
-  const handleLogin = async (event,email,password) => {
+  const handleLogin = async (event, email, password) => {
     event.preventDefault();
     try {
-      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
       const user = userCredential.user;
-      navigate("/Dashboard")
-      setEmail('');
-      setPassword('');
+      navigate("/Dashboard");
+      setEmail("");
+      setPassword("");
     } catch (error) {
-      alert('Login failed. Please check your credentials.');
+      alert("Login failed. Please check your credentials.");
       console.error(error);
     }
   };
-  const handleLoginFirst = async (event,email,password) => {
+  const handleLoginFirst = async (event, email, password) => {
     event.preventDefault();
     try {
-      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
       const currentUser = auth.currentUser;
-    if (currentUser) {
-      const userData = {
-        UID: currentUser.uid,
-        email: currentUser.email,
-        name: currentUser.displayName || 'Default Name', // Use display name or a default name if not available
-      };
+      if (currentUser) {
+        const userData = {
+          UID: currentUser.uid,
+          email: currentUser.email,
+          name: currentUser.displayName || "Default Name", // Use display name or a default name if not available
+        };
 
-      // Make a POST request to your API using Axios to create user data entry
-      axios.post('http://localhost:1337/api/userdata', { data: userData })
-        .then((response) => {
-          console.log('Entry created:', response.data);
-          navigate('/Dashboard')
-        })
-        .catch((error) => {
-          console.error('Error creating entry:', error);
-          // Show error notification for entry creation
-          alert('Error creating user data entry. Please try again.');
-        });
-    } else {
-      // If currentUser is null, show error notification
-      alert('User not found. Please log in again.');
-    }
-      setEmail('');
-      setPassword('');
+        // Make a POST request to your API using Axios to create user data entry
+        axios
+          .post("http://localhost:1337/api/userdata", { data: userData })
+          .then((response) => {
+            console.log("Entry created:", response.data);
+            navigate("/Dashboard");
+          })
+          .catch((error) => {
+            console.error("Error creating entry:", error);
+            // Show error notification for entry creation
+            alert("Error creating user data entry. Please try again.");
+          });
+      } else {
+        // If currentUser is null, show error notification
+        alert("User not found. Please log in again.");
+      }
+      setEmail("");
+      setPassword("");
     } catch (error) {
-      alert('Login failed. Please check your credentials.');
+      alert("Login failed. Please check your credentials.");
       console.error(error);
     }
   };
 
   const [showResetModal, setShowResetModal] = useState(false);
-  const [emailForReset, setEmailForReset] = useState('');
+  const [emailForReset, setEmailForReset] = useState("");
 
   const handleResetLinkClick = () => {
     setShowResetModal(true);
@@ -112,14 +116,14 @@ const AuthForm = ({ formType, handleSubmit, background }) => {
     event.preventDefault();
     try {
       await sendPasswordResetEmail(auth, emailForReset);
-      alert('Password reset email sent!');
-      setEmailForReset('');
+      alert("Password reset email sent!");
+      setEmailForReset("");
       setShowResetModal(false);
     } catch (error) {
       // Handle specific error when email doesn't exist
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   return (
     <form
@@ -134,8 +138,8 @@ const AuthForm = ({ formType, handleSubmit, background }) => {
             type="name"
             placeholder="Full Name"
             value={name}
-            onChange={(e) => setName(e.target.value)}  
-            required                                 
+            onChange={(e) => setName(e.target.value)}
+            required
             name="fullName"
             className="bg-[#eee] focus-within:border-black border-2 my-2 px-4 py-3 text-sm rounded-lg w-full transition-all duration-500"
           />
@@ -186,28 +190,30 @@ const AuthForm = ({ formType, handleSubmit, background }) => {
       )}
       {showResetModal && (
         <div className="fixed flex items-center justify-center h-full px-6 md:px-10 gap-2 z-100 inset-0 bg-opacity-50">
-            <button
-              className={`absolute top-4 left-8 shadow-[0_0_5px_rgba(0,0,0,.5)] active:shadow-[inset_0_0_5px_rgba(0,0,0,.5)] rounded-xl py-2 px-4 mt-8 md:mt-4 active:scale-[.98] ${`bg-[#240046] text-white hover:bg-white hover:text-[#240046]`}`}
-              onClick={hideModal}
+          <button
+            className={`absolute top-4 left-8 shadow-[0_0_5px_rgba(0,0,0,.5)] active:shadow-[inset_0_0_5px_rgba(0,0,0,.5)] rounded-xl py-2 px-4 mt-8 md:mt-4 active:scale-[.98] ${`bg-[#240046] text-white hover:bg-white hover:text-[#240046]`}`}
+            onClick={hideModal}
+          >
+            {/* Back arrow icon */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              {/* Back arrow icon */}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M15 19l-7-7 7-7"
-                />
-              </svg>
-            </button>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M15 19l-7-7 7-7"
+              />
+            </svg>
+          </button>
           <div className="bg-white p-8 rounded-md max-w-md w-full h-full flex flex-col items-center md:justify-center">
-            <h2 className="font-bold text-4xl mb-4 text-center">Reset Password</h2>
+            <h2 className="font-bold text-4xl mb-4 text-center">
+              Reset Password
+            </h2>
             <form className="flex flex-col items-center md:justify-center">
               <input
                 type="email"
@@ -227,18 +233,23 @@ const AuthForm = ({ formType, handleSubmit, background }) => {
           </div>
         </div>
       )}
-      {formType === "signUp" ? 
-      <button
-        onClick={(event) => handleRegister(event, name, email, password)}
-        className={`shadow-[0_0_5px_rgba(0,0,0,.5)] active:shadow-[inset_0_0_5px_rgba(0,0,0,.5)] rounded-xl py-2 px-8 mt-8 md:mt-4 active:scale-[.98] ${`bg-[#240046] text-white hover:bg-white hover:text-[#240046]`}`}
-      > Sign Up
+      {formType === "signUp" ? (
+        <button
+          onClick={(event) => handleRegister(event, name, email, password)}
+          className={`shadow-[0_0_5px_rgba(0,0,0,.5)] active:shadow-[inset_0_0_5px_rgba(0,0,0,.5)] rounded-xl py-2 px-8 mt-8 md:mt-4 active:scale-[.98] ${`bg-[#240046] text-white hover:bg-white hover:text-[#240046]`}`}
+        >
+          {" "}
+          Sign Up
         </button>
-         :
-         <button
-         onClick={(event) => handleLogin(event, email, password)}
-         className={`shadow-[0_0_5px_rgba(0,0,0,.5)] active:shadow-[inset_0_0_5px_rgba(0,0,0,.5)] rounded-xl py-2 px-8 mt-8 md:mt-4 active:scale-[.98] ${`bg-[#240046] text-white hover:bg-white hover:text-[#240046]`}`}
-       > Sign In
-         </button>}
+      ) : (
+        <button
+          onClick={(event) => handleLogin(event, email, password)}
+          className={`shadow-[0_0_5px_rgba(0,0,0,.5)] active:shadow-[inset_0_0_5px_rgba(0,0,0,.5)] rounded-xl py-2 px-8 mt-8 md:mt-4 active:scale-[.98] ${`bg-[#240046] text-white hover:bg-white hover:text-[#240046]`}`}
+        >
+          {" "}
+          Sign In
+        </button>
+      )}
     </form>
   );
 };
@@ -301,10 +312,7 @@ const Login = () => {
   };
 
   return (
-    <div
-      className={`flex items-center justify-center h-screen`}
-      style={{ background: background_color }}
-    >
+    <div className={`flex items-center justify-center h-screen bg-[#f2ebfb]`}>
       {/* Particles */}
       <div className="fixed">
         <ParticleSphere
@@ -339,7 +347,7 @@ const Login = () => {
         {introData.map((item, index) => (
           <div
             key={index}
-            className={`md:hidden absolute w-full h-max text-center text-white flex flex-col items-center justify-center gap-2 transition-all duration-300 px-6 py-2 z-[2] ${
+            className={`md:hidden absolute w-full h-max text-center text-white bg-[#240046] flex flex-col items-center justify-center gap-2 transition-all duration-300 px-6 py-2 z-[2] ${
               item.title !== "Welcome Back !"
                 ? clicked
                   ? "bottom-full"
@@ -348,7 +356,6 @@ const Login = () => {
                 ? "top-0"
                 : "top-full"
             }`}
-            style={{ background: cont_color }}
           >
             <h1 className="font-bold text-2xl">{item.title}</h1>
             <p>{item.description}</p>
@@ -411,10 +418,9 @@ const Login = () => {
           }`}
         >
           <div
-            className={`h-full text-white relative w-[200%] transition-all duration-500 -left-full ${
+            className={`h-full text-white bg-[#240046] relative w-[200%] transition-all duration-500 -left-full ${
               clicked ? "translate-x-1/2" : ""
             }`}
-            style={{ background: cont_color }}
           >
             {introData.map((item) => (
               <div
