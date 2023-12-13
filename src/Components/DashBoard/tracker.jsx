99916e2f5704a26b6fde3819742fbad50db0d11c
Arham <ar.hayat7@gmail.com>
2023-12-14 03:26:44 +0530

    Authentication

--- src/Components/DashBoard/tracker.jsx
+++ src/Components/DashBoard/tracker.jsx
@@ -1,8 +1,42 @@
 import React from "react";
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { auth } from '../../firebase';
 
 function Tracker() {
+  const location = useLocation();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        // User is signed in.
+        setUser(user);
+      } else {
+        // No user is signed in.
+        setUser(null);
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
   return (
-    <div className="glassmorph rounded-xl shadow-[0_0_5px_rgba(0,0,0,.2)] md:mb-8 sticky top-0 md:static"></div>
+    <div className="glassmorph p-4 rounded-xl shadow-[0_0_5px_rgba(0,0,0,.2)] md:mb-8 sticky top-0 md:static">
+      <h1 className="text-3xl font-bold mb-4">Welcome to the Dashboard</h1>
+      {user && (
+        <div>
+          <p className="text-lg">
+            <span className="font-bold">Display Name:</span> {user.displayName}
+          </p>
+          <p className="text-lg">
+            <span className="font-bold">Email:</span> {user.email}
+          </p>
+          {/* You can display other user information here */}
+        </div>
+      )}
+    </div>
   );
 }
 
--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,9 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Navbar, Collapse } from "@material-tailwind/react";
 import { HiOutlineMenuAlt3 } from "react-icons/hi";
 import { IoCloseCircle } from "react-icons/io5";
+import { auth } from './../firebase'; 
+
 
 const Nav = ({ navbarData }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // This effect runs on component mount
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+
+    // Clean up the observer when component unmounts
+    return () => unsubscribe();
+  }, []);
+  
   const { navItems, buttonText } = navbarData;
   const [openNav, setOpenNav] = useState(false);
 
@@ -54,12 +68,18 @@ const Nav = ({ navbarData }) => {
 
         {/* Nav-end */}
         <div className="flex items-center gap-4 p-2">
-          <a
+        {user ? (
+        // If user is logged in, display Log Out button
+        <button className={`bg-gray-800 rounded-lg p-1 px-4 md:rounded-none md:px-1 md:bg-transparent md:border-0 text-white md:text-black text-[16px] relative before:absolute before:bg-black before:bottom-0 before:left-0 before:w-[100%] before:h-[4%] lg:before:hover:scale-x-100 before:origin-left before:duration-500 before:scale-x-0 ${navbarData.class}`} onClick={() => auth.signOut()}>Log Out</button>
+      ) : (
+        // If user is not logged in, display Log In button
+        <a
             href="/Login"
             className={`bg-gray-800 rounded-lg p-1 px-4 md:rounded-none md:px-1 md:bg-transparent md:border-0 text-white md:text-black text-[16px] relative before:absolute before:bg-black before:bottom-0 before:left-0 before:w-[100%] before:h-[4%] lg:before:hover:scale-x-100 before:origin-left before:duration-500 before:scale-x-0 ${navbarData.class}`}
           >
             Log In
           </a>
+      )}
           <button className="hidden lg:inline-block py-2 px-4 rounded-xl bg-black text-white hover:bg-white hover:text-black drop-shadow-md duration-500">
             <span>{buttonText}</span>
           </button>
