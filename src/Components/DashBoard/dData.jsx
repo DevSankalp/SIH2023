2db1a9d35397f5e5b353cede7083324bb4581683
DevSankalp <94849180+DevSankalp@users.noreply.github.com>
2023-12-17 12:01:26 +0530

    Tracker Added

--- src/Components/DashBoard/dData.jsx
+++ src/Components/DashBoard/dData.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 import Tracker from "./tracker";
+import Slider from "react-slick";
+import ChapterData from "./chapters.json";
 
 function Data() {
+  const settings = {
+    arrows: true,
+    fade: true,
+  };
+
   return (
-    <div className="w-full h-full grid grid-rows-4 md:grid-rows-[30%_70%] pl-16 p-2 md:p-8 duration-500">
+    <div className="w-full h-full grid grid-rows-4 md:grid-rows-[40%_60%] pl-16 p-2 md:p-8 duration-500">
       <div className="glassmorph rounded-xl shadow-[0_0_5px_rgba(0,0,0,.2)] bg-[rgba(255,255,255,.7)] mb-8">
         <Tracker />
       </div>
-      <div className="glassmorph rounded-xl shadow-[0_0_5px_rgba(0,0,0,.2)] bg-[rgba(255,255,255,.7)]"></div>
+      <div className="w-full flex items-center justify-center gap-8">
+        <div className="glassmorph rounded-xl shadow-[0_0_5px_rgba(0,0,0,.2)] bg-[rgba(255,255,255,.7)] w-full h-full">
+          <Slider {...settings}>
+            {/* {ChapterData.map((items, index) => (
+              <div key={index}>{items.Chapters}</div>
+            ))} */}
+            {console.log(ChapterData)}
+          </Slider>
+        </div>
+        <div className="glassmorph rounded-xl shadow-[0_0_5px_rgba(0,0,0,.2)] bg-[rgba(255,255,255,.7)] w-full h-full"></div>
+        <div className="glassmorph rounded-xl shadow-[0_0_5px_rgba(0,0,0,.2)] bg-[rgba(255,255,255,.7)] w-full h-full"></div>
+      </div>
     </div>
   );
 }
--- src/Components/DashBoard/tracker.jsx
+++ src/Components/DashBoard/tracker.jsx
@@ -1,12 +1,105 @@
 import React, { useState, useEffect } from "react";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCol,
+  MDBContainer,
+  MDBIcon,
+  MDBRow,
+  MDBTypography,
+} from "mdb-react-ui-kit";
 
 const Tracker = () => {
   return (
-    <div className="flex items-center justify-center w-full h-full z-[0]">
-      {/* <div className="w-max h-full flex items-center justify-center">
-        <div className="w-24 h-24 border-8 border-blue-500 rounded-full bg-black"></div>
-        <div className="border-8 border-blue-500 w-32 h-12 bg-blue-500"></div>
-      </div> */}
+    <div className="z-[0]">
+      <section className="vh-100">
+        <MDBContainer className="py-5 h-100">
+          <MDBRow className="justify-content-center align-items-center h-100">
+            <MDBCol size="12">
+              <MDBCard
+                className="card-stepper text-black"
+                style={{ borderRadius: "16px" }}
+              >
+                <MDBCardBody className="p-5">
+                  <div className="d-flex justify-content-between align-items-center mb-5">
+                    <div>
+                      <MDBTypography tag="h5" className="mb-0">
+                        INVOICE{" "}
+                        <span className="text-primary font-weight-bold">
+                          #Y34XDHR
+                        </span>
+                      </MDBTypography>
+                    </div>
+                    <div className="text-end">
+                      <p className="mb-0">
+                        Expected Arrival <span>01/12/19</span>
+                      </p>
+                      <p className="mb-0">
+                        USPS{" "}
+                        <span className="font-weight-bold">
+                          234094567242423422898
+                        </span>
+                      </p>
+                    </div>
+                  </div>
+                  <ul
+                    id="progressbar-2"
+                    className="d-flex justify-content-between mx-0 mt-0 mb-5 px-0 pt-0 pb-2"
+                  >
+                    <li className="step0 active text-center" id="step1"></li>
+                    <li className="step0 active text-center" id="step2"></li>
+                    <li className="step0 active text-center" id="step3"></li>
+                    <li className="step0 text-muted text-end" id="step4"></li>
+                  </ul>
+
+                  <div className="d-flex justify-content-between">
+                    <div className="d-lg-flex align-items-center">
+                      <MDBIcon
+                        fas
+                        icon="clipboard-list me-lg-4 mb-3 mb-lg-0"
+                        size="3x"
+                      />
+                      <div>
+                        <p className="fw-bold mb-1">Order</p>
+                        <p className="fw-bold mb-0">Processed</p>
+                      </div>
+                    </div>
+                    <div className="d-lg-flex align-items-center">
+                      <MDBIcon
+                        fas
+                        icon="box-open me-lg-4 mb-3 mb-lg-0"
+                        size="3x"
+                      />
+                      <div>
+                        <p className="fw-bold mb-1">Order</p>
+                        <p className="fw-bold mb-0">Shipped</p>
+                      </div>
+                    </div>
+                    <div className="d-lg-flex align-items-center">
+                      <MDBIcon
+                        fas
+                        icon="shipping-fast me-lg-4 mb-3 mb-lg-0"
+                        size="3x"
+                      />
+                      <div>
+                        <p className="fw-bold mb-1">Order</p>
+                        <p className="fw-bold mb-0">En Route</p>
+                      </div>
+                    </div>
+                    <div className="d-lg-flex align-items-center">
+                      <MDBIcon fas icon="home me-lg-4 mb-3 mb-lg-0" size="3x" />
+                      <div>
+                        <p className="fw-bold mb-1">Order</p>
+                        <p className="fw-bold mb-0">Arrived</p>
+                      </div>
+                    </div>
+                  </div>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </section>
     </div>
   );
 };
