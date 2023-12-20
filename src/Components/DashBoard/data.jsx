2b1ffd01e018a504911ae7b20ef54b99c2db74e5
DevSankalp <94849180+DevSankalp@users.noreply.github.com>
2023-12-20 17:33:57 +0530

    damnnn!!

--- src/Components/DashBoard/data.jsx
+++ src/Components/DashBoard/data.jsx
@@ -52,6 +52,52 @@ const Carousel = () => {
 };
 
 function Data() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const [next, setNext] = useState("");
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionClick = (option) => {
+    setSelectedOption(option.label);
+    setNext(option.value);
+    setIsOpen(false); // Close dropdown when an option is clicked
+  };
+
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const options = [
+    { value: "A", label: "Grant of Approval for New Institution" },
+    {
+      value: "B",
+      label: "Grant of Extension of Approval for Existing Institutions.",
+    },
+    { value: "C", label: "Collaboration & Twinning Programmes" },
+    { value: "D", label: "Grant of Approval for Universities" },
+    {
+      value: "E",
+      label:
+        "Approval for Open and Distance Learning (ODL) / Online Learning (OL)",
+    },
+  ];
+
+  const links = [
+    {
+      "Forgotten Password":
+        "https://www.aicte-india.org/sites/default/files/approval/2023-24/Affidavit-1.pdf",
+      "Application Format":
+        "https://www.aicte-india.org/sites/default/files/approval/2023-24/Affidavit-2.pdf",
+      "Completion Closure":
+        "https://www.aicte-india.org/sites/default/files/approval/2023-24/Affidavit-4.pdf",
+      "Release of Security Deposit":
+        "https://www.aicte-india.org/sites/default/files/approval/2023-24/Affidavit-3.pdf",
+      "Additional course(s)/intake":
+        "https://www.aicte-india.org/sites/default/files/approval/2023-24/Affidavit-8.pdf",
+    },
+  ];
+
   const [active, setActive] = useState(false);
 
   return (
@@ -78,7 +124,51 @@ function Data() {
           <Carousel />
         </div>
         <div className="glassmorph rounded-xl shadow-[0_0_5px_rgba(0,0,0,.2)] bg-[rgba(255,255,255,.7)] w-full h-full"></div>
-        <div className="glassmorph rounded-xl shadow-[0_0_5px_rgba(0,0,0,.2)] bg-[rgba(255,255,255,.7)] w-full h-full"></div>
+        <div className="glassmorph rounded-xl shadow-[0_0_5px_rgba(0,0,0,.2)] bg-[rgba(255,255,255,.7)] w-full h-full">
+          <button
+            id="dropdownDividerButton"
+            onClick={toggleDropdown}
+            className="block appearance-none bg-white border border-gray-300 hover:border-gray-400 px-4 py-2 pr-8 rounded-lg shadow-sm focus:outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 min-w-20"
+            style={{ minWidth: "250px" }}
+          >
+            {selectedOption || "Select an option"}
+            <svg
+              className={`absolute inset-y-0 right-0 w-5 h-5 mt-2 mr-3 pointer-events-none ${
+                isOpen ? "transform rotate-180" : ""
+              } text-gray-400 transition-transform duration-300`}
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 12a1 1 0 0 1-.707-.293l-4-4a1 1 0 1 1 1.414-1.414L10 10.586l3.293-3.293a1 1 0 1 1 1.414 1.414l-4 4A1 1 0 0 1 10 12z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+          {isOpen && (
+            <ul
+              className="absolute z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-full"
+              aria-labelledby="dropdownDividerButton"
+            >
+              {options.map((option) => (
+                <li key={option.value}>
+                  <button
+                    className="block px-4 py-2 w-full text-left hover:bg-gray-100 focus:outline-none"
+                    onClick={() => handleOptionClick(option)}
+                  >
+                    {option.label}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          )}
+          {links.map((items, index) => (
+            <a key={index} href={items[1]}>
+              {items[0]}
+            </a>
+          ))}
+        </div>
       </div>
     </div>
   );
